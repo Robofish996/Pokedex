d63664e6e05ddac7988246b68227c147d389ef51
Robofish996 <Matthew.A.Riddle@outlook.com>
2023-02-15 00:35:08 +0200

    Completed Pokedex

--- scripts/main.js
+++ scripts/main.js
@@ -2,22 +2,14 @@ function logic() {
 
     // First thing to do is get input from user
     let name = document.getElementById("name");
-
     let type = document.getElementById("type");
-
     let region = document.getElementById("region");
 
-
-    // Will only give objects
-    console.log(name + " - " + type + " - " + region + " region") // remove once you have clear understanding
 
     // Get String values of variables
     name = name.value;
     type = type.value;
     region = region.value;
-
-    // Can log them since they are now pure Strings
-    console.log(name + " - " + type + " - " + region + " region") // remove once you have clear understanding
 
 
     /*-----------------------------------------------
@@ -25,13 +17,13 @@ function logic() {
      *---------------------------------------------*/
 
     /* --- Part 1 : Create pokemon data Object --- */
-
     // Use If to ensure input validation (ensure input fields are !empty)
 
     if (name === "" || type === "" || region === "") {
-        alert("All fields must be entered");
+        alert("Please make sure all fields are populated!")
         return false;
     }
+
 
     // Create pokemon object here (const pokemon) using user input Strings
 
@@ -39,14 +31,13 @@ function logic() {
         name: name,
         type: type,
         region: region,
-    };
-    console.log(pokemon);
+    }
+
 
     // Create a span element (const pokemonData) and put the pokemon objects properties inside of it
 
     const pokemonData = document.createElement("span");
-    pokemonData.innerHTML = `Name: ${pokemon.name} Type: ${pokemon.type} Region: ${pokemon.region}`;
-    document.body.appendChild(pokemonData);
+    pokemonData.innerHTML = `Name: ${pokemon.name} Type: ${pokemon.type} Region:${pokemon.region}`;
 
     /* --- Part 2 : Creating other DOM Objects --- */
 
@@ -55,63 +46,43 @@ function logic() {
     const pokedexEntry = document.createElement("li");
 
 
-    // Give pokedexEntry (li) a new class equal to the "type" property of the pokemon object
 
+    // Give pokedexEntry (li) a new class equal to the "type" property of the pokemon object
     pokedexEntry.className = pokemon.type;
 
-
-
     // Creating a preview button (using a Template Literal)
-
-    const previewButton = `
-    <form action="http://google.com/search" target="_blank">
-        <input name="q" hidden value="${pokemon.name}">
-        <input type="submit">
-    </form>
-`;
-    pokedexEntry.innerHTML = previewButton;
-    const pokedexEntries = document.getElementById("pokedex-entries");
-    pokedexEntries.appendChild(pokedexEntry);
-    
-
     // this code is for an html form that will Google search the name of the pokemon
+    const previewButton = `
+            <form action="http://google.com/search" target="_blank">
+                <input name="q" hidden value="${name}">
+                <input type="submit" value="Preview">
+            </form>
+        `
 
 
+    /* --- Part 3 : Add things into the DOM --- */
 
+    // Firstly append pokemonData to the innerHTML of pokedexEntry
 
+    pokedexEntry.innerHTML += pokemonData.outerHTML;
 
+    // Secondly append the previewButton to innerHTML of pokedexEntry
+    pokedexEntry.innerHTML += previewButton;
 
+    // Lastly we will get the ul List element that is meant to contain all pokedex entries
+    const pokedexEntries = document.getElementById("pokedex-entries");
+    
+    // The we will append pokedexEntry to the innerHTML of the pokedexEntries ul element
+    pokedexEntries.appendChild(pokedexEntry);
 
+    // Add pokemon object to pokedexMemory array
+    const pokedexMemory = [];
+    pokedexMemory.push(pokemon);
 
+    // Log the pokedexMemory array to the console
+    console.log(pokedexMemory);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    /*-----------------------------------------------
+     * Solution must be above this comment
+     *---------------------------------------------*/
 }
